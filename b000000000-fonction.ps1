14b0a2ab8d91872f05fd841a481e2ea7b538e4eb
didier300110500 <guigma_d@yahoo.fr>
2020-04-04 22:49:46 -0400

    .

--- b000000000-fonction.ps1
+++ b000000000-fonction.ps1
@@ -1,30 +1,31 @@
 ﻿<#
+
 .SYNOPSIS
+
     Ce script est un laboratoire Powershell
-
 .DESCRIPTION
+
     Ce script est utilisé pour le laboratoire de programmation en Powershell.
-
 .NOTES
-    Author: CollegeBoreal
+
+    Author: AEKchaouche
     Derniere mise à jour: yyyy-mm-dd
-
 #>
-
 # Definition de la fonction
 function Stagiaire {
+   
+    param (
+           [String]$personneNom,
+           [Int]$personneAge
+      )
 
-param (
-        [String]$personneNom,
-        [Int]$personneAge
-    )
-}
+       
+   
+    # message de bienvenue 
 
-
-
-# message de bienvenue 
     "Bonjour {0} ! Tu as {1} ans." -F $personneNom, $personneAge
+  }
 
-# Appel de la fonction
-Stagiaire --VILLE-- --AGE--
-
+   # Appel de la fonction
+   Stagiaire Toronto 35
+ 
